d3ccb5d93b750406f1a16e0b9c0f0a1d415c3ab0
Hemanth328 <hemanth.blueburn11@gmail.com>
2023-10-18 09:28:19 +0530

    Saving Object by enhancing methods

---
+++ HBProj02-EnhancedClientApp/src/main/java/com/org/hbp/HbPersistObject.java
@@ -0,0 +1,70 @@
+package com.org.hbp;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.org.entity.Product;
+
+
+// Try with resource 7/8 version
+
+public class HbPersistObject {
+	
+    public static void main( String[] args ) {
+    	
+      // Bootstrap/activate the Hibernate
+    	Configuration cfg = new Configuration();
+    	
+    	//specifying the Hibernate Configuration file name and location
+    	cfg.configure("com/org/cfgs/hibernate.cfg.xml");
+
+    	
+    	Transaction tx = null;
+    	boolean flag = false;
+    	
+    	try(SessionFactory factory = cfg.buildSessionFactory();
+    			
+    			Session ses = factory.openSession();) {
+    		// begin transaction
+    		tx = ses.beginTransaction(); // internally calls con.setAutoCommit(false) to disable auto commit mode on DB s/w 
+    		// Prepare entity object
+    		
+    		Product prod = new Product();
+    		
+//    		prod.setPid(1001);
+    		prod.setPname("Chairs");
+    		prod.setPrice(11456.33f);
+    		prod.setQty(6.0f);
+    		
+    		// save object
+//    		Integer idval = (Integer)ses.save(prod); // gives persistence instruction to save the object (insert object data as the record) this returns the id value
+
+    		ses.persist(prod); // gives persistence instruction to save the object (insert object data as the record) this returns the id value
+    		
+    		tx.commit(); // internally calls con.commit method to make insertion execution results permanent
+//    		
+//    		System.out.println("Generator retuned Id value is : "+idval);
+    		
+    		System.out.println("Object is saved (Record is inserted)");
+    		
+    		
+    		flag = true;
+    		
+    	}
+    	catch(HibernateException he) {
+    		he.printStackTrace();
+//    		
+    		if(tx!= null && tx.getRollbackOnly() && tx.getStatus() != null) {
+    			tx.rollback();
+    			System.out.println("Object is not saved");
+    		}
+    		
+
+    	}
+    	
+        
+    }
+}
---
+++ HBProj02-EnhancedClientApp/src/test/java/Hibernate/HBProj02_EnhancedClientApp/AppTest.java
@@ -0,0 +1,20 @@
+package Hibernate.HBProj02_EnhancedClientApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
